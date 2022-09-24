08f588ff8a3d3be527b07bd60dc8ffc4824ba331
MiguelGarcia98GitHub <100066250+MiguelGarcia98GitHub@users.noreply.github.com>
2022-09-24 18:52:26 +0200

    First Commit: CRUD functionality done, files are controllers, database, services and v1 routes

---
+++ src/services/workoutService.js
@@ -0,0 +1,38 @@
+const { v4: uuid } = require("uuid");
+const Workout = require("../database/Workout");
+
+const getAllWorkouts = () => {
+  const allWorkouts = Workout.getAllWorkouts();
+  return allWorkouts;
+};
+const getOneWorkout = (workoutId) => {
+  const workout = Workout.getOneWorkout(workoutId);
+  return workout;
+};
+const createNewWorkout = (newWorkout) => {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString("en-US", { timezone: "UTC" }),
+    updatedAt: new Date().toLocaleString("en-US", { timezone: "UTC" }),
+  };
+
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+  return createdWorkout;
+};
+const updateOneWorkout = (workoutId, changes) => {
+  const updatedWorkout = Workout.updateOneWorkout(workoutId, changes);
+  return updatedWorkout;
+};
+const deleteOneWorkout = (workoutId) => {
+  Workout.deleteOneWorkout(workoutId);
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
